e354741673c5bbfdb3553545626be450a378efb6
dangdung2352637 <dangdungone@gmail.com>
2024-07-27 15:01:37 +0700

    commit 27-7

--- src/pages/About.js
+++ src/pages/About.js
@@ -1,5 +1,36 @@
 import "./About.scss";
+import Bixplorer_banner from "../image/Bixplorer_banner.png";
 
 export const About = () => {
-  return <div className="About">website about</div>;
+  return (
+    <div className="About">
+      <div className="About-1">
+        <div className="About-1-content">
+          <h1>HI-END VENTURES DEX ECOSYSTEM</h1>
+          <h3>
+            Smart investments in exclusively <br></br> top-notch projects
+          </h3>
+          <div className="About-1-button">
+            <button>XPP Private Sale</button>
+            <button>Get a 15% bonus</button>
+          </div>
+        </div>
+        <img width="500" height="600" src={Bixplorer_banner} />
+      </div>
+
+      <div className="About-2">
+        <p className="About-2-1">
+          All actions are fully guaranteed by WEB 3.0 smart contracts,
+          eliminating human intervention and ensuring security.
+        </p>
+        <p className="About-2-2">
+          Simplifying investment through technologies and innovative products
+          that promote Web3 adoption
+        </p>
+      </div>
+
+      <div>hello 2</div>
+      <div>hello 2</div>
+    </div>
+  );
 };
--- src/pages/Page1.js
+++ src/pages/Page1.js
@@ -4,6 +4,9 @@ export const Page1 = () => {
   return (
     <div>
       <div>hello world</div>
+      <div>hello 2</div>
+      <div>hello 2</div>
+      <div>hello 2</div>
     </div>
   );
 };
